c69c742db1569b3832404da33ca87d4567ee0b6e
Galleazzo <paulogalleazzo1@gmail.com>
2025-01-11 09:00:09 -0300

    feat: add cors configuration class

---
+++ src/main/java/com/br/erp/web/config/CorsFilterConfig.java
@@ -0,0 +1,36 @@
+package com.br.erp.web.config;
+
+import org.springframework.boot.web.servlet.FilterRegistrationBean;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.core.Ordered;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+import org.springframework.web.filter.CorsFilter;
+
+import java.util.Arrays;
+import java.util.List;
+
+@Configuration
+public class CorsFilterConfig {
+
+    public static final List<String> allowedOrigins = Arrays.asList("http://localhost:4200", "https://pacotinho-do-amor.vercel.app");
+
+    @Bean
+    public FilterRegistrationBean<CorsFilter> initCorsFilter() {
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        CorsConfiguration config = new CorsConfiguration();
+
+        config.setAllowCredentials(true);
+        config.setAllowedHeaders(Arrays.asList("Authorization", "Cache-Control", "Content-Type"));
+        config.addAllowedMethod("*"); // Permite todos os métodos (GET, POST, etc.)
+        config.setAllowedOrigins(allowedOrigins); // Permite requisições de localhost e do seu site em produção
+
+        // Aplica a configuração de CORS para todas as rotas
+        source.registerCorsConfiguration("/**", config);
+
+        FilterRegistrationBean<CorsFilter> bean = new FilterRegistrationBean<>(new CorsFilter(source));
+        bean.setOrder(Ordered.HIGHEST_PRECEDENCE); // Prioriza este filtro
+        return bean;
+    }
+}
